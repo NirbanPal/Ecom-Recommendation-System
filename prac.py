fd10b70a5fa14efac6344a29bf644dbb47220d53
NirbanPal <paulnirban007@gmail.com>
2023-04-26 00:57:37 +0530

    all done

---
+++ prac.py
@@ -0,0 +1,14 @@
+import re
+
+# Input string
+input_string = "images\\296565635120.jpg"
+
+# Regular expression pattern to match the number between backslash and .jpg
+
+# Search for the pattern in the input string
+
+# Extract the number from the match object
+if match:
+    print("Number:", number)
+else:
+    print("No match found.")
